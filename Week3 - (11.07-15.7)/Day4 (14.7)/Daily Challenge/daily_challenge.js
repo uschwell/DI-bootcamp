078bc59819042bcf0c80a030bcc16e530feeb4dc
Uri <schwelluri@gmail.com>
2021-07-14 20:25:42 +0200

    Finished mini-project1 and MOST of the daily challenge (doesnt finish crossing out on check/uncheck)

--- Week3 - (11.07-15.7)/Day4 (14.7)/Daily Challenge/daily_challenge.js
+++ Week3 - (11.07-15.7)/Day4 (14.7)/Daily Challenge/daily_challenge.js
@@ -1,30 +1,47 @@
 const taskList= document.getElementById('toDoList');
-
+let listTasks = [];
 
 function addTask(){
 
 	//get the input text
 	let input=document.getElementById("inputBox").value;
-	//reset the text box for next time
-	document.getElementById("inputBox").value="";
+	if(input!=""){
+		//reset the text box for next time
+		document.getElementById("inputBox").value="";
 
-	//note: (these indentations are for my ease of use of the code)
-	//create a new overall div container to add to  the list
-	let temp = document.createElement("DIV");
-					temp.setAttribute("class", "listItem");
-	//create a text div to save the input text
-	let textBox= document.createElement("DIV");
-					//save the input text in our new div
-					textBox.textContent = input;
+		//note: (these indentations are for my ease of use of the code)
+		//create a new overall div container to add to  the list
+		let temp = document.createElement("DIV");
+						temp.setAttribute("class", "listItem");
+		//create a text div to save the input text
+		let textBox= document.createElement("DIV");
+						//save the input text in our new div
+						textBox.textContent = input;
 
-	//create a check div to add a checkmark
-	let checkBox = document.createElement("input");
-	checkBox.setAttribute("type", "checkBox");
+		//create a check div to add a checkmark
+		let checkBox = document.createElement("input");
+		checkBox.setAttribute("type", "checkBox");
+		checkBox.style.font='awesome';
+		checkBox.addEventListener('checked', cross_out);
 
-	//add the checkbox AND the textBox to our overall div
-	temp.appendChild(checkBox);
-	temp.appendChild(textBox);
+		//add the checkbox AND the textBox to our overall div
+		temp.appendChild(checkBox);
+		temp.appendChild(textBox);
 
-	//finally, we add our overall div to the to-do list
-	taskList.appendChild(temp);
+
+		listTasks.push(input);
+
+		//finally, we add our overall div to the to-do list
+		taskList.appendChild(temp);
+	}
+
+}
+
+
+function cross_out(e){
+	e.setAttribute.checked='true';
+}
+
+function cross_in(e){
+	e.setAttribute("style", "del");
 }
