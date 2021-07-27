ae4f30a3cbf7e56dc5655a38df2cae42191753d0
Uri <schwelluri@gmail.com>
2021-07-27 10:40:31 +0200

    _

--- Week5 (25.7-29.7)/Day2 (26.7)/Daily Challenge/Daily_Challenge.py
+++ Week5 (25.7-29.7)/Day2 (26.7)/Daily Challenge/Daily_Challenge.py
@@ -1,10 +1,9 @@
-from _typeshed import Self
+
 import random
 
-class Gene():
-    thisGene = True
-
-    def _init_(self,gene):
+class Gene:
+    thisGene=(-1)
+    def __init__(self,gene):
         #upon generation-there is a 50/50 chance the given value will 'mutate'
         #if we did mutate
         if(random.randint(0, 1)):
@@ -31,21 +30,102 @@ class Chromosome(Gene):
     #     gene6.thisGene,gene7.thisGene,gene8.thisGene,gene9.thisGene,]
 
 
-    def _init_(self):
+    def __init__(self):
         #I will be generating the chromosomes randomly- if you wish to make it specific you can use the commented out constructor above
         #it is possible to generate a random "flip" here as well- but I see no point in repeating it-since we already generate it randomly 
         # AND generate each Gene with a random chance at mutation
         for i in range(10):
             temp=random.randint(0,1)
+
             temp2=Gene(temp)
             self.thisChromo.append(temp2)
 
+    def mutateChromo(self, mutations):
+        counter=0
+        while(counter<mutations):
+            #we pick a random gene in the chromosome
+            pick=random.randint(0,9)
+            #once we pick the gene-we have a 50/50 chance of it mutating
+            if(random.randint(0,1)):
+                self.thisChromo[pick].mutateGene()
+
+            counter+=1
 
 
-class DNA():
+
+class DNA(Chromosome):
     myDNA=[]
-    def _init_(self, chromo):
+    def __init__(self, chromo):
         self.myDNA=chromo
 
     def dnaAdd(self, chromo):
-        self.myDNA.append(chromo)
+        (myDNA).append(chromo)
+
+        def mutateDNA(self, mutations):
+            counter=0
+            #pick a random Chromosome to mutate
+            picker=random.randint(0,(len(self.myDNA)-1))
+            #there is a 50/50 chance that the chromosome will mutate at all
+            if(random.randint(0, 1)):
+                #we send the chromosome to go mutate (as defined by the earlier class)
+                (self.myDNA[picker]).mutateChromo(mutations)
+
+
+class Organism(DNA):
+    def __init__(self, dna, environment):
+        self.dna=dna
+        self.environment=environment
+
+    def lets_mutate(self):
+        for chrom in self.dna:
+            chrom.mutateDNA(self.environment)
+
+#Note: I don't know if this is what you want- but each time I create a chromosome- I automatically generate 10 random genes
+#if that is not what's wanted- switch to the commented out code in the Chromosome class
+
+# chrom1=Chromosome()
+# print("output")
+
+# #print out the chromosome
+# toPrint=(chrom1.thisChromo)
+# for chrom in toPrint:
+#     print(str(chrom.thisGene)+" ", end="")
+
+# chrom1.mutateChromo(10)
+# print("")
+
+# #print out the mutated chromosome
+# toPrint=(chrom1.thisChromo)
+# for chrom in toPrint:
+#     print(str(chrom.thisGene)+" ", end="")
+
+
+#Note: I deliberatley did NOT automate this- wasn't sure what was required- this shows the other optional way
+chrom1=Chromosome()
+chrom2=Chromosome()
+chrom3=Chromosome()
+chrom4=Chromosome()
+chrom5=Chromosome()
+myDNA=DNA(chrom1)
+
+
+# myDNA.dnaAdd(chrom2)
+# myDNA.dnaAdd(chrom3)
+# myDNA.dnaAdd(chrom4)
+# myDNA.dnaAdd(chrom5)
+#create an organism
+org1=Organism(myDNA, 5)
+flag=1
+counter=0
+while(flag):
+    counter+=1
+    org1.lets_mutate()
+    for dn in org1:
+        for chrom in dn:
+            flag=10
+            for gen in chrom:
+                flag-=(gen.thisGene)
+            if(flag==0):
+                break
+
+print("It took "+str(counter)+" generations")
