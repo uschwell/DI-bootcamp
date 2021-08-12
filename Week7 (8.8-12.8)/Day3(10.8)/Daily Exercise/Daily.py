161ffaeeb30449788a1f18b513f431f8a8044bb2
Uri <schwelluri@gmail.com>
2021-08-12 16:16:34 +0200

    daily ex from 10.8

--- Week7 (8.8-12.8)/Day3(10.8)/Daily Exercise/Daily.py
+++ Week7 (8.8-12.8)/Day3(10.8)/Daily Exercise/Daily.py
@@ -1,30 +1,42 @@
 import numpy as np
+import pandas as pd
+import sklearn
+# from sklearn.neighbors import KNeighborsClassifier
+# from sklearn.model_selection import train_test_split
+
+# x= np.arrange(10).reshape(5,2)
+
+# y=['green', 'red', 'yellow',]
 
 
-x= np.arrange(10).reshape(5,2)
-
-y=['green', 'red', 'yellow',]
-
-
-labeled={}
-for i in range(len(y)):
-    labeled[y[i]]=x[i]
+# labeled={}
+# for i in range(len(y)):
+#     labeled[y[i]]=x[i]
 
 
 
 
 class KNN:
-    __init__(self, k_nearest):
-        self.k_nearest=k_nearest
+    def __init__(self, n):
+        self.n=n
 
 
-    def fit(X_train, Y_train):
-        labeled={}
-    for i in range(len(y)):
-        labeled[y[i]]=x[i]
+    def fit(self, X_train, Y_train):
+        self.X_train= X_train
+        self.Y_train= Y_train
 
-print("hello")
-
+    def predict(self, X_test):
+        y_guess = []
+        for test in X_test:
+            distances=[]
+            for rows in X_test:
+                dist = np.linalg.norm(test - rows)
+                distances.append(dist)
+            indices = list(np.argpartition(distances, self.n)[:self.n])
+            ind_lables= self.Y_train.iloc[indices]
+            count=ind_lables.value_counts()
+            max_val=count.idxmax()
+            y_guess.append(max_val)
 
 
 
